e8b7bb8c2607550bec5960b95c7e55e994ba716e
Rebeca Cruz <rebecacruz@me.com>
2020-10-25 13:00:02 -0300

    Criacao do CursoRepository e TopicoForm, melhorias nas classes Topico e TopicoController

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,23 +1,45 @@
 package br.com.alura.forum.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
-import br.com.alura.forum.modelo.Curso;
+import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.CursoRepository;
+import br.com.alura.forum.repository.TopicoRepository;
 
 @RestController
+@RequestMapping("/topicos")
 public class TopicosController {
-
 	
-	@RequestMapping("/topicos")
-	public List<TopicoDto> lista(){
-		Topico topico = new Topico("Dúvida","Dúvida com Spring", new Curso("Spring", "Programação"));
-		
-		return TopicoDto.converter(Arrays.asList(topico, topico,topico));
+	@Autowired
+	private TopicoRepository topicoRepository;
+	
+	@Autowired
+	private CursoRepository cursoRepository;
+	
+	@GetMapping
+	public List<TopicoDto> lista(String nomeCurso){
+		if (nomeCurso == null) {
+			List<Topico> topicos = topicoRepository.findAll();
+			return TopicoDto.converter(topicos);
+		} else {
+			List<Topico> topicos = topicoRepository.findByCurso_Nome(nomeCurso);
+					return TopicoDto.converter(topicos);
+		}		
 	}
+	
+	@PostMapping
+	public void cadastrar(@RequestBody TopicoForm form) {
+		Topico topico = form.converter(cursoRepository);
+		topicoRepository.save(topico);
+	}
+			
 }
